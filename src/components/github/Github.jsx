efdc986b64934cfbe34558dc6f78135e5c19f3ea
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-08-20 22:37:51 +0530

    feat: github section theme change with site theme toggle

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import GitHubCalendar from "react-github-calendar";
+import { useTheme } from "../../context/ThemeContext";
 import "./github.css";
 
 const Github = () => {
+  const { isDarkMode } = useTheme();
   return (
     <div className="git-wrapper">
       <GitHubCalendar
         username="abhimax"
         blockSize={16}
         blockMargin={5}
-        colorScheme="dark"
+        colorScheme={isDarkMode ? "dark" : "light"}
         fontSize={16}
         showTotalCount={true}
         style={{ width: "100%", maxWidth: 700, margin: "0 auto" }}
