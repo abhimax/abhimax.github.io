d50edb99a734871b902c3064ecb558d2ae534652
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-08-30 15:50:30 +0530

    feat: use github style custom hook

--- src/components/github/Github.jsx
+++ src/components/github/Github.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import GitHubCalendar from "react-github-calendar";
 import { useTheme } from "../../context/ThemeContext";
+import useResponsiveGithubCalendar from "../../hooks/useResponsiveGithubCalendar";
 import "./github.css";
 
 const Github = () => {
   const { isDarkMode } = useTheme();
+  const { blockSize, fontSize, blockMargin } = useResponsiveGithubCalendar();
   return (
     <div className="git-wrapper">
       <GitHubCalendar
         username="abhimax"
-        blockSize={16}
-        blockMargin={5}
+        blockSize={blockSize}
+        blockMargin={blockMargin}
         colorScheme={isDarkMode ? "dark" : "light"}
-        fontSize={16}
+        fontSize={fontSize}
         showTotalCount={true}
         style={{ width: "100%", maxWidth: 700, margin: "0 auto" }}
       />
