3aff7a22d66b0a076e4dfe4f2297b12e904afcca
Abhiman Ranaweera <abhiman.max@gmail.com>
2025-09-06 23:28:33 +0530

    feat: update testimonial

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,51 +3,7 @@ import Slider from "react-slick";
 import "./testimonials.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const testimonials = [
-  {
-    name: "Kelum Prasad",
-    designation: "Senior Software Engineer, 99x",
-    comment:
-      "I have worked with Abhiman more than 4 years in two companies. He is a technically sound, result oriented character. His good analytical knowledge, along with the technical knowledge helped us on implementing some cool android based products and web based solutions. And his creativity polished the final product. A team player with great attitudes.",
-    image: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "Nadeesha Perera",
-    designation: "UI/UX Designer, Interblocks",
-    comment:
-      "Abhiman is a passionate developer who always delivers on time. His attention to detail and creativity are top notch. He is a great asset to any team.",
-    image: "https://randomuser.me/api/portraits/women/44.jpg",
-  },
-  {
-    name: "Sahan Fernando",
-    designation: "Tech Lead, MadMobile",
-    comment:
-      "Working with Abhiman was a pleasure. He is a quick learner and adapts to new technologies easily. Highly recommended!",
-    image: "https://randomuser.me/api/portraits/men/45.jpg",
-  },
-  {
-    name: "Ishara Jayasuriya",
-    designation: "Product Manager, Multplx",
-    comment:
-      "Abhiman's leadership and technical skills are impressive. He always motivates the team and brings out the best in everyone.",
-    image: "https://randomuser.me/api/portraits/women/68.jpg",
-  },
-  {
-    name: "Ruwan Dias",
-    designation: "DevOps Engineer, SyscoLabs",
-    comment:
-      "His problem-solving skills and positive attitude make him a valuable team member. Always ready to help and share knowledge.",
-    image: "https://randomuser.me/api/portraits/men/51.jpg",
-  },
-  {
-    name: "Tharushi Senanayake",
-    designation: "Frontend Developer, MadMobile",
-    comment:
-      "Abhiman is creative, reliable, and always up for a challenge. He brings energy and innovation to every project.",
-    image: "https://randomuser.me/api/portraits/women/52.jpg",
-  },
-];
+import testimonials from "../../data/testimonialsData";
 
 const settings = {
   dots: true,
